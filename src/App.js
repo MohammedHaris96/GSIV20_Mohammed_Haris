d8facfd255d24ec926b7a6660405ed40f75617a6
Haris <mhaaris0815@gmail.com>
2022-01-25 23:02:15 +0530

    all files added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./index.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Container">
+        <div className="movie-card">
+          <div className="movie-card-text">
+            <label>Movie Title</label>
+            <label>Rating</label>
+            {/* <label>Description</label> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
